290867d94402514d32be24ef1991a673be52fa46
Suwandi The <144767691+Suwandithe@users.noreply.github.com>
2023-12-16 22:05:10 +0700

    updated

--- login.js
+++ login.js
@@ -13,9 +13,3 @@ loginLink.addEventListener('click', ()=>{
     wrapper.classList.remove('active');
 });
 
-btnPopup.addEventListener('click', ()=>{
-    wrapper.classList.add('active-popup');
-});
-iconClose.addEventListener('click', ()=>{
-    wrapper.classList.remove('active-popup');
-});
